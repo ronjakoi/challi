86dbfe3995187e969480c37a3b438ebbea73478f
Ronja Koistinen <ronja.koistinen@kapsi.fi>
2017-08-01 15:12:37 +0300

    Getting started with Click

--- build_blog.py
+++ build_blog.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
-import sqlite3
-import locale
-import re
+import sqlite3, locale, re, click
 from os import makedirs, path
 from markdown import markdown
 from datetime import datetime
@@ -41,11 +39,6 @@ def geturi(filename, pd):
 def pubdate2str(pubdate, formatstr):
     pd = datetime.strptime(pubdate, "%Y-%m-%d %H:%M:%S")
     return pd.strftime(formatstr)
-
-conn = sqlite3.connect(pybb)
-conn.row_factory = sqlite3.Row
-cur = conn.cursor()
-cur.execute("PRAGMA foreign_keys=1")
 
 def getsummary(content):
     """
@@ -89,7 +82,7 @@ def makeindex():
 
     makedirs(outdir, exist_ok=True)
     idxf = open(outdir + "/index.html", 'w+', encoding="utf-8")
-    
+
     idxf.write(header)
     cur.execute("SELECT post_id, title, publish_date, filename, content "
                 "FROM posts ORDER BY publish_date DESC LIMIT ?",
@@ -109,9 +102,9 @@ def makeindex():
 
         idxf.write("<p>Luokat: {}</p>\n".format(gettagsline(row["post_id"])))
 
-        if debug: print(".", end="")
+        if debug: click.echo(".", end="")
     idxf.write(footer)
-    if debug: print("")
+    if debug: click.echo("")
     idxf.close()
 
 def writeposts():
@@ -132,8 +125,8 @@ def writeposts():
             f.write("<p>Luokat: {}</p>\n".
                     format(gettagsline(row["post_id"], "../../")))
             f.write(footer)
-        if debug: print(".", end="")
-    if debug: print("")
+        if debug: click.echo(".", end="")
+    if debug: click.echo("")
 
 def makefullidx():
     """Make an index page listing all posts"""
@@ -156,11 +149,11 @@ def makefullidx():
                  )
                 )
         prevmonth = thismonth
-        if debug: print(".", end="")
+        if debug: click.echo(".", end="")
     f.write("</ul>")
     f.write(footer)
     f.close()
-    if debug: print("")
+    if debug: click.echo("")
 
 def maketagindex():
     """Make alphabetical list of all tags"""
@@ -175,9 +168,9 @@ def maketagindex():
                            "GROUP BY tags_ref.tag_id ORDER BY text ASC"):
         f.write("<li><a href=\"tag/%s.html\">%s</a>"
                 " &mdash; %d posts" % (row["text"], row["text"], row["count"]))
-        if debug: print(".", end="")
+        if debug: click.echo(".", end="")
     f.write("</ul>")
-    if debug: print("")
+    if debug: click.echo("")
 
 def maketagpages():
     """Make a page for each tag"""
@@ -214,24 +207,152 @@ def maketagpages():
         tagfiles[tagpath].write("<p>Luokat: {}</p>\n".
                                 format(gettagsline(row["post_id"], "../")))
 
-        if debug: print(".", end="")
+        if debug: click.echo(".", end="")
     for p, f in tagfiles.items():
         f.write(footer)
         f.close()
-    if debug: print("")
+    if debug: click.echo("")
 
-# Invoke functions from here
+# Click stuff
 
-if debug: print("Writing posts ", end = "")
-writeposts()
-if debug: print("Creating index.html ", end = "")
-makeindex()
-if debug: print("Creating all_posts.html ", end = "")
-makefullidx()
-if debug: print("Creating tag/*.html ", end = "")
-maketagpages()
-if debug: print("Creating all_tags.html ", end = "")
-maketagindex()
+@click.group(context_settings=dict(help_option_names=['-h', '--help']))
+@click.version_option()
+def cli():
+    """A static blog generator."""
+    pass
 
-if debug: print("Done.")
-conn.close()
+@click.command()
+@click.argument("dir",
+                 required=False,
+                 type=click.Path(dir_okay=True, writable=True))
+def init(dir):
+    """Initialize a new blog.
+
+    This creates an empty database file.
+    By default the current working directory is used,
+    but you can optionally provide a different one.
+    """
+    if not dir: dir = "."
+    click.echo("Initializing a new blog in `%s'" % dir)
+
+@click.command()
+@click.option('--hidden', is_flag=True,
+               help="Make this post hidden (a draft).")
+@click.option('--input', '-i', type=click.File('r'),
+               help="Read post content from file (don't open an editor).")
+def post(hidden, input):
+    """Write a new blog post."""
+    click.echo("Make a new post")
+
+@click.command(name="ls")
+@click.option('--order-by',
+               type=click.Choice(['id', 'title', 'date']),
+               help="Select which field to order by (default=date).")
+@click.option('--asc','collation', flag_value='ASC', default=False,
+               help="Ascending order.")
+@click.option('--desc','collation', flag_value='DESC', default=True,
+               help="Descending order (default).")
+def list_posts(order_by, collation):
+    """List all blog posts."""
+
+    # Get terminal width and height
+    tw, th = click.get_terminal_size()
+    rowstr = "{:>6} | {:<19} | {:<25}"
+    # Print a '-' separator with '+' signs at column borders,
+    # fill to terminal width
+    separator = "-"*6 + "-+-" + "-"*19 + "-+-"
+    separator += "-"*(tw-len(separator))
+    query = "SELECT post_id, publish_date, title FROM posts ORDER BY publish_date DESC"
+    if order_by == "date" or order_by is None:
+        order_by = "publish_date"
+    click.echo("query was:\n" + query)
+    click.echo(rowstr.format("ID", "Date", "Title"))
+    click.echo(separator)
+    for row in cur.execute(query):#,(order_by,)):
+        click.echo(rowstr.format(row["post_id"], row["publish_date"],
+                   row["title"]))
+
+@click.command()
+@click.argument('id', type=click.INT)
+def edit(id):
+    """Edit a post with given ID."""
+
+    postquery = "SELECT title, content FROM posts WHERE post_id = ?"
+    cur.execute(postquery,(id,))
+    try:
+        title, content = cur.fetchone()
+    except:
+        raise click.BadParameter("No post found with ID %d" % id)
+    tagquery = """SELECT text AS tag FROM tags, tags_ref WHERE
+    tags_ref.post_id = ? AND tags.tag_id = tags_ref.tag_id
+    ORDER BY tag"""
+    cur.execute(tagquery, (id,))
+    tagsline = "Luokat: "
+    tagslist = (r[0] for r in cur.fetchall())
+    if tagslist is not None:
+        tagsline += ", ".join(tagslist)
+    print(tagsline)
+    content += tagsline
+    new_content = click.edit(text=(title + "\n" + content),
+        extension=".md", require_save=True)
+
+    if new_content is not None:
+        click.echo("Congrats, you've made an edit!")
+    else:
+        click.echo("Edit aborted.")
+
+@click.command()
+@click.argument('id', type=click.INT)
+def hide(id):
+    """Flag a post with given ID as hidden."""
+    click.echo("Hide a post")
+
+@click.command()
+@click.argument('id', type=click.INT)
+def unhide(id):
+    """Flag a post with given ID as not hidden."""
+    click.echo("Unhide a post")
+
+@click.command()
+def publish():
+    """Upload the blog.
+
+    Copies the blog to the configured
+    location using rsync."""
+    click.echo("Publish a post")
+
+@click.command()
+@click.argument('id', type=click.INT)
+def rm():
+    """Remove a post with given ID. The post is deleted from both
+    the directory tree and the database.
+
+    Remember that you can also hide posts. This retains the data in
+    the database, in case you want to use it again later.
+    """
+
+@click.command()
+def rebuild():
+    """Rebuild all posts and tags.
+
+    This can be used to recreate
+    the blog from e.g. a database backup if something's gone wrong."""
+
+cli.add_command(post)
+cli.add_command(list_posts)
+cli.add_command(edit)
+cli.add_command(hide)
+cli.add_command(unhide)
+cli.add_command(publish)
+cli.add_command(rm)
+cli.add_command(rebuild)
+cli.add_command(init)
+
+if __name__ == '__main__':
+    conn = sqlite3.connect(pybb)
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    cur.execute("PRAGMA foreign_keys=1")
+    cli()
+    conn.close()
+
