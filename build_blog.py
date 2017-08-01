70e2eb92f9447878e829154cb83a559918f39e7f
Ronja Koistinen <ronja.koistinen@kapsi.fi>
2017-08-01 18:05:52 +0300

    Put the old functions under rebuild() and made progress bars

--- build_blog.py
+++ build_blog.py
@@ -87,24 +87,22 @@ def makeindex():
     cur.execute("SELECT post_id, title, publish_date, filename, content "
                 "FROM posts ORDER BY publish_date DESC LIMIT ?",
                 (index_len,))
-    for row in cur.fetchall():
-        pdstring = pubdate2str(row["publish_date"], dateformat)
-        outfile = geturi(row["filename"], row["publish_date"])
-        is_summary, summary = getsummary(row["content"])
-        idxf.write("<h3><a href=\"{outfile}\">{title}</a></h3>\n"
-                   "<p>{publish_date}</p>\n{summary}"
-                    .format(outfile=outfile,
-                            publish_date=pdstring,
-                            title=row["title"],
-                            summary=summary))
-        if is_summary: idxf.write("<p><a href=\"{}\">Read more...</a></p>\n"
-                                  .format(outfile))
+    with click.progressbar(cur.fetchall(), label="Making index.html", width=0) as posts:
+        for row in posts:
+            pdstring = pubdate2str(row["publish_date"], dateformat)
+            outfile = geturi(row["filename"], row["publish_date"])
+            is_summary, summary = getsummary(row["content"])
+            idxf.write("<h3><a href=\"{outfile}\">{title}</a></h3>\n"
+                       "<p>{publish_date}</p>\n{summary}"
+                        .format(outfile=outfile,
+                                publish_date=pdstring,
+                                title=row["title"],
+                                summary=summary))
+            if is_summary: idxf.write("<p><a href=\"{}\">Read more...</a></p>\n"
+                                      .format(outfile))
 
-        idxf.write("<p>Luokat: {}</p>\n".format(gettagsline(row["post_id"])))
-
-        if debug: click.echo(".", end="")
+            idxf.write("<p>Luokat: {}</p>\n".format(gettagsline(row["post_id"])))
     idxf.write(footer)
-    if debug: click.echo("")
     idxf.close()
 
 def writeposts():
@@ -112,21 +110,20 @@ def writeposts():
 
     cur.execute("SELECT post_id, title, publish_date, filename, content "
                 "FROM posts")
-    for row in cur.fetchall():
-        pdstring = pubdate2str(row["publish_date"], dateformat)
-        datedir = path.join(row["publish_date"][0:4], row["publish_date"][5:7])
-        makedirs(path.join(outdir, datedir), mode=0o750, exist_ok=True)
-        outfile = path.join(outdir, datedir, row["filename"])
-        # Write each post file
-        with open(outfile, 'w', encoding="utf-8") as f:
-            f.write(header)
-            f.write("<h1>" + row["title"] + "</h1>\n" + "<p>" + pdstring + "</p>\n")
-            f.write(markdown(row["content"]))
-            f.write("<p>Luokat: {}</p>\n".
-                    format(gettagsline(row["post_id"], "../../")))
-            f.write(footer)
-        if debug: click.echo(".", end="")
-    if debug: click.echo("")
+    with click.progressbar(cur.fetchall(), label="Writing posts", width=0) as posts:
+        for row in posts:
+            pdstring = pubdate2str(row["publish_date"], dateformat)
+            datedir = path.join(row["publish_date"][0:4], row["publish_date"][5:7])
+            makedirs(path.join(outdir, datedir), mode=0o750, exist_ok=True)
+            outfile = path.join(outdir, datedir, row["filename"])
+            # Write each post file
+            with open(outfile, 'w', encoding="utf-8") as f:
+                f.write(header)
+                f.write("<h1>" + row["title"] + "</h1>\n" + "<p>" + pdstring + "</p>\n")
+                f.write(markdown(row["content"]))
+                f.write("<p>Luokat: {}</p>\n".
+                        format(gettagsline(row["post_id"], "../../")))
+                f.write(footer)
 
 def makefullidx():
     """Make an index page listing all posts"""
@@ -135,25 +132,27 @@ def makefullidx():
     f = open(outdir + "/all_posts.html", 'w', encoding="utf-8")
     f.write(header)
     prevmonth = None
-    for row in cur.execute("SELECT title, publish_date, filename "
-                           "FROM posts ORDER BY publish_date DESC"):
-        pd = datetime.strptime(row["publish_date"], "%Y-%m-%d %H:%M:%S")
-        thismonth = (pd.year, pd.month)
-        # if prevmonth is None: f.write("<ul>\n");
-        if thismonth != prevmonth:
-            if prevmonth is not None: f.write("</ul>\n")
-            f.write("<h2>" + pubdate2str(row["publish_date"], "%B %Y") + "</h2>\n<ul>")
-        f.write("<li><a href=\"%s\">%s</a> &mdash; %s</li>" %
-                (geturi(row["filename"], row["publish_date"]),
-                 row["title"], pubdate2str(row["publish_date"], dateformat)
-                 )
-                )
-        prevmonth = thismonth
-        if debug: click.echo(".", end="")
+    cur.execute("SELECT title, publish_date, filename "
+                "FROM posts ORDER BY publish_date DESC")
+
+    with click.progressbar(cur.fetchall(),
+    label="Making all_posts.html", width=0) as posts:
+        for row in posts:
+            pd = datetime.strptime(row["publish_date"], "%Y-%m-%d %H:%M:%S")
+            thismonth = (pd.year, pd.month)
+            # if prevmonth is None: f.write("<ul>\n");
+            if thismonth != prevmonth:
+                if prevmonth is not None: f.write("</ul>\n")
+                f.write("<h2>" + pubdate2str(row["publish_date"], "%B %Y") + "</h2>\n<ul>")
+            f.write("<li><a href=\"%s\">%s</a> &mdash; %s</li>" %
+                    (geturi(row["filename"], row["publish_date"]),
+                     row["title"], pubdate2str(row["publish_date"], dateformat)
+                     )
+                    )
+            prevmonth = thismonth
     f.write("</ul>")
     f.write(footer)
     f.close()
-    if debug: click.echo("")
 
 def maketagindex():
     """Make alphabetical list of all tags"""
@@ -162,15 +161,16 @@ def maketagindex():
     f = open(outdir + "/all_tags.html", 'w', encoding="utf-8")
     f.write(header)
     f.write("<ul>")
-    for row in cur.execute("SELECT text, COUNT(tags_ref.tag_id) as count "
-                           "FROM tags, tags_ref "
-                           "WHERE tags.tag_id = tags_ref.tag_id "
-                           "GROUP BY tags_ref.tag_id ORDER BY text ASC"):
-        f.write("<li><a href=\"tag/%s.html\">%s</a>"
-                " &mdash; %d posts" % (row["text"], row["text"], row["count"]))
-        if debug: click.echo(".", end="")
+    cur.execute("SELECT text, COUNT(tags_ref.tag_id) as count "
+                "FROM tags, tags_ref "
+                "WHERE tags.tag_id = tags_ref.tag_id "
+                "GROUP BY tags_ref.tag_id ORDER BY text ASC")
+    with click.progressbar(cur.fetchall(), label="Making all_tags.html",
+    width=0) as tags:
+        for row in tags:
+            f.write("<li><a href=\"tag/%s.html\">%s</a>"
+                    " &mdash; %d posts" % (row["text"], row["text"], row["count"]))
     f.write("</ul>")
-    if debug: click.echo("")
 
 def maketagpages():
     """Make a page for each tag"""
@@ -186,32 +186,31 @@ def maketagpages():
                 "WHERE tags_ref.post_id = posts.post_id "
                 "AND tags_ref.tag_id = tags.tag_id "
                 "ORDER BY tags.text ASC, posts.publish_date DESC")
-    for row in cur.fetchall():
-        tagpath = path.join(tagdir, row["tag"] + ".html")
-        if tagpath not in tagfiles.keys():
-            tagfiles[tagpath] = open(tagpath, 'w')
-            tagfiles[tagpath].write(header)
-        postpath = "../" + geturi(row["fn"],  row["pd"])
-        pdstring = pubdate2str(row["pd"], dateformat)
-        has_summary, summary = getsummary(row["content"])
-        tagfiles[tagpath].write("<h3><a href=\"{outfile}\">{title}</a></h3>\n"
-                       "<p>{publish_date}</p>\n{summary}\n"
-                       .format(outfile=postpath,
-                                publish_date=pdstring,
-                                title=row["title"],
-                                summary=summary))
-        if has_summary: tagfiles[tagpath].write(
-            "<p><a href=\"{}\">Read more...</a></p>\n"
-            .format(postpath))
+    with click.progressbar(cur.fetchall(), label="Making tag_*.html", width=0) \
+    as tags:
+        for row in tags:
+            tagpath = path.join(tagdir, row["tag"] + ".html")
+            if tagpath not in tagfiles.keys():
+                tagfiles[tagpath] = open(tagpath, 'w')
+                tagfiles[tagpath].write(header)
+            postpath = "../" + geturi(row["fn"],  row["pd"])
+            pdstring = pubdate2str(row["pd"], dateformat)
+            has_summary, summary = getsummary(row["content"])
+            tagfiles[tagpath].write("<h3><a href=\"{outfile}\">{title}</a></h3>\n"
+                           "<p>{publish_date}</p>\n{summary}\n"
+                           .format(outfile=postpath,
+                                    publish_date=pdstring,
+                                    title=row["title"],
+                                    summary=summary))
+            if has_summary: tagfiles[tagpath].write(
+                "<p><a href=\"{}\">Read more...</a></p>\n"
+                .format(postpath))
 
-        tagfiles[tagpath].write("<p>Luokat: {}</p>\n".
-                                format(gettagsline(row["post_id"], "../")))
-
-        if debug: click.echo(".", end="")
+            tagfiles[tagpath].write("<p>Luokat: {}</p>\n".
+                                    format(gettagsline(row["post_id"], "../")))
     for p, f in tagfiles.items():
         f.write(footer)
         f.close()
-    if debug: click.echo("")
 
 # Click stuff
 
@@ -337,6 +336,11 @@ def rebuild():
 
     This can be used to recreate
     the blog from e.g. a database backup if something's gone wrong."""
+    writeposts()
+    makeindex()
+    makefullidx()
+    maketagpages()
+    maketagindex()
 
 @click.command()
 @click.argument('file', nargs=-1)
