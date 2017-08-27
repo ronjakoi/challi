4218992cea7e52f994dfbb65bc7a6e1eb1bbf747
Ronja Koistinen <ronja.koistinen@kapsi.fi>
2017-08-27 14:27:03 +0300

    Config stuff

--- challi.py
+++ challi.py
@@ -20,6 +20,10 @@ break_re = r'[*-_]( *[*-_]){2,}'
 config_file = "config.ini"
 blog_conf = None
 cur = None
+conn = None
+index_len = None
+header, footer = "", ""
+
 
 def geturi(filename: str, pd: str) -> str:
     """Get a post's URI. Arguments are the post's filename and publish_date.
@@ -54,6 +58,7 @@ def getsummary(content: str) -> Tuple:
 def gettagsline(post_id: int, prefix: str = "") -> str:
     """Get the tags for a post by id"""
 
+    global conn
     cur_inner = conn.cursor()
     cur_inner.execute("SELECT tags.text AS tag FROM tags, posts, tags_ref "
                       "WHERE tags.tag_id = tags_ref.tag_id AND "
@@ -69,7 +74,9 @@ def split_input(post_text: str) -> Tuple:
         if i == 0:
             title = line.strip()
         elif line.startswith("{}: "):
-            tags = line.strip().replace("{} ".format(tags_text), "", 1).split(", ")
+            tags = line.strip(). \
+                replace("{} ".format(blog_conf["template"]["tags_line_header"]), "", 1). \
+                split(", ")
         else:
             body += line + "\n"
     return title, body, tags
@@ -78,10 +85,10 @@ def split_input(post_text: str) -> Tuple:
 def makeindex():
     """Make the main index.html"""
 
-    makedirs(outdir, exist_ok=True)
-    idxf = open(path.join(outdir,
-                          config.get("files", "index_file", fallback="index.html")),
-                          'w+', encoding="utf-8")
+    makedirs(blog_conf["files"]["blog_dir"], exist_ok=True)
+    idxf = open(path.join(blog_conf["files"]["blog_dir"],
+                          blog_conf.get("files", "index_file", fallback="index.html")),
+                'w+', encoding="utf-8")
 
     idxf.write(header)
     cur.execute("SELECT post_id, title, publish_date, filename, content "
@@ -89,7 +96,8 @@ def makeindex():
                 (index_len,))
     with click.progressbar(cur, label="Making index.html", width=0) as posts:
         for row in posts:
-            pdstring = pubdate2str(row["publish_date"], dateformat)
+            pdstring = pubdate2str(row["publish_date"],
+                                   blog_conf["template"]["date_format"])
             outfile = geturi(row["filename"], row["publish_date"])
             is_summary, summary = getsummary(row["content"])
             idxf.write("<h3><a href=\"{outfile}\">{title}</a></h3>\n"
@@ -101,8 +109,10 @@ def makeindex():
             if is_summary:
                 idxf.write("<p><a href=\"{}\">{}</a></p>\n"
                            .format(outfile,
-                                   config.get("template", "read_more", fallback="Read more...")))
-            idxf.write("<p>{} {}</p>\n".format(tags_text, gettagsline(row["post_id"])))
+                                   blog_conf.get("template", "read_more", fallback="Read more...")))
+            idxf.write("<p>{} {}</p>\n".
+                       format(blog_conf["template"]["tags_line_header"],
+                              gettagsline(row["post_id"])))
     idxf.write(footer)
     idxf.close()
 
@@ -114,17 +124,19 @@ def writeposts():
                 "FROM posts")
     with click.progressbar(cur, label="Writing posts", width=0) as posts:
         for row in posts:
-            pdstring = pubdate2str(row["publish_date"], dateformat)
+            pdstring = pubdate2str(row["publish_date"],
+                                   blog_conf["template"]["date_format"])
             datedir = path.join(row["publish_date"][0:4], row["publish_date"][5:7])
-            makedirs(path.join(outdir, datedir), mode=0o750, exist_ok=True)
-            outfile = path.join(outdir, datedir, row["filename"])
+            makedirs(path.join(blog_conf["files"]["blog_dir"], datedir),
+                     mode=0o750, exist_ok=True)
+            outfile = path.join(blog_conf["files"]["blog_dir"], datedir, row["filename"])
             # Write each post file
             with open(outfile, 'w', encoding="utf-8") as f:
                 f.write(header)
                 f.write("<h1>" + row["title"] + "</h1>\n" + "<p>" + pdstring + "</p>\n")
                 f.write(markdown(row["content"]))
                 f.write("<p>{} {}</p>\n".
-                        format(config.get("template", "tags_line_header", fallback="Tags:"),
+                        format(blog_conf.get("template", "tags_line_header", fallback="Tags:"),
                                gettagsline(row["post_id"], "../../")))
                 f.write(footer)
 
@@ -132,9 +144,9 @@ def writeposts():
 def makefullidx():
     """Make an index page listing all posts."""
 
-    makedirs(outdir, exist_ok=True)
-    archive_index = config.get("files", "archive_index", fallback="all_posts.html")
-    f = open(path.join(outdir, archive_index),
+    makedirs(blog_conf["files"]["blog_dir"], exist_ok=True)
+    archive_index = blog_conf.get("files", "archive_index", fallback="all_posts.html")
+    f = open(path.join(blog_conf["files"]["blog_dir"], archive_index),
              'w', encoding="utf-8")
     f.write(header)
     prevmonth = None
@@ -152,7 +164,8 @@ def makefullidx():
             f.write("<li><a href=\"%s\">%s</a> &mdash; %s</li>" %
                     (geturi(row["filename"], row["publish_date"]),
                      row["title"],
-                     pubdate2str(row["publish_date"], dateformat))
+                     pubdate2str(row["publish_date"],
+                                 blog_conf["template"]["date_format"]))
                     )
             prevmonth = thismonth
     f.write("</ul>" + footer)
@@ -162,9 +175,9 @@ def makefullidx():
 def maketagindex():
     """Make alphabetical list of all tags."""
 
-    makedirs(outdir, exist_ok=True)
-    tag_index = config.get("files", "tags_index", fallback="all_tags.html")
-    f = open(path.join(outdir, tag_index), 'w', encoding="utf-8")
+    makedirs(blog_conf["files"]["blog_dir"], exist_ok=True)
+    tag_index = blog_conf.get("files", "tags_index", fallback="all_tags.html")
+    f = open(path.join(blog_conf["files"]["blog_dir"], tag_index), 'w', encoding="utf-8")
     f.write(header)
     f.write("<ul>")
     cur.execute("SELECT text, COUNT(tags_ref.tag_id) as count "
@@ -175,7 +188,7 @@ def maketagindex():
         for row in tags:
             f.write("<li><a href=\"tag/%s.html\">%s</a>"
                     " &mdash; %d %s" % (row["text"], row["text"], row["count"],
-                                        config.get("template", "tags_posts", fallback="posts")))
+                                        blog_conf.get("template", "tags_posts", fallback="posts")))
     f.write("</ul>")
     f.close()
 
@@ -183,7 +196,7 @@ def maketagindex():
 def maketagpages():
     """Make a page for each tag."""
 
-    tagdir = path.join(outdir, "tag")
+    tagdir = path.join(blog_conf["files"]["blog_dir"], "tag")
     makedirs(tagdir, exist_ok=True)
     tagfiles = {}
     cur.execute("SELECT tags.text AS tag, posts.title AS title, "
@@ -201,7 +214,7 @@ def maketagpages():
                 tagfiles[tagpath] = open(tagpath, 'w')
                 tagfiles[tagpath].write(header)
             postpath = "../" + geturi(row["fn"], row["pd"])
-            pdstring = pubdate2str(row["pd"], dateformat)
+            pdstring = pubdate2str(row["pd"], blog_conf["template"]["date_format"])
             has_summary, summary = getsummary(row["content"])
             tagfiles[tagpath].write("<h3><a href=\"{outfile}\">{title}</a></h3>\n"
                                     "<p>{publish_date}</p>\n{summary}\n"
@@ -214,7 +227,8 @@ def maketagpages():
                                         .format(postpath))
 
             tagfiles[tagpath].write("<p>{} {}</p>\n".
-                                    format(tags_text, gettagsline(row["post_id"], "../")))
+                                    format(blog_conf["template"]["tags_line_header"],
+                                           gettagsline(row["post_id"], "../")))
     for f in tagfiles.values():
         f.write(footer)
         f.close()
@@ -246,6 +260,7 @@ def db_rm_orphan_tags():
 
 def set_post_hidden(id_: int, hidden: bool):
     """Set the hidden status of a post."""
+    global conn
     cur.execute("UPDATE posts SET hidden = ? WHERE id = ?", (hidden, id_))
     conn.commit()
 
@@ -270,12 +285,18 @@ def cli(config):
     blog_conf.read(config_file, encoding="utf-8")
     date_locale = blog_conf.get("template", "date_locale", fallback="C")
     locale.setlocale(locale.LC_ALL, date_locale)
-    dateformat = blog_conf.get("template", "date_format", fallback="%B %d, %Y")
+    blog_conf["template"]["date_format"] = blog_conf.get("template",
+                                                         "date_format",
+                                                         fallback="%B %d, %Y")
+    global index_len
     index_len = blog_conf.getint("files", "number_of_index_articles", fallback=8)
-    outdir = blog_conf.get("files", "blog_dir", fallback=".")
-    tags_text = blog_conf.get("template", "tags_line_header", fallback="Tags:")
+    blog_conf["files"]["blog_dir"] = blog_conf.get("files", "blog_dir", fallback=".")
+    blog_conf["template"]["tags_line_header"] = blog_conf.get("template",
+                                                              "tags_line_header",
+                                                              fallback="Tags:")
     header_file = blog_conf.get("files", "header_file", fallback=None)
     footer_file = blog_conf.get("files", "footer_file", fallback=None)
+    global header, footer
     if header_file:
         with open(header_file, "r", encoding="utf-8") as hf:
             header = hf.read()
@@ -296,11 +317,13 @@ def cli(config):
 
     try:
         # Setting up Sqlite connection
-        with sqlite3.connect(db_file) as conn:
-            global cur
-            conn.row_factory = sqlite3.Row
-            cur = conn.cursor()
-            cur.execute("PRAGMA foreign_keys=1")
+        global conn
+        conn = sqlite3.connect(db_file)
+        global cur
+
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+        cur.execute("PRAGMA foreign_keys=1")
     except sqlite3.IntegrityError as e:
         click.echo("SQL error: %s" % e)
 
@@ -373,6 +396,8 @@ def init(directory):
     """
 
     init_cur.execute(init_sql)
+    init_conn.commit()
+    init_conn.close()
 
 
 @click.command()
@@ -386,7 +411,7 @@ def post(hidden, get_from):
     post_template = \
         ("This line is your title\n\n"
          "The body of your post goes here.\n\n"
-         "{} comma-separated, list, of, tags").format(tags_text)
+         "{} comma-separated, list, of, tags").format(blog_conf["template"]["tags_line_header"])
 
     query = """INSERT INTO posts
             (title, content, publish_date, filename, hidden)
@@ -405,7 +430,7 @@ def post(hidden, get_from):
     title, body, tags = split_input(post_text)
 
     fromchars = "äöåøæđðčžš"
-    tochars   = "aoaoaddczs"
+    tochars = "aoaoaddczs"
     transtable = str.maketrans(fromchars, tochars)
 
     filename = title.replace(" ", "_").lower()
@@ -438,15 +463,15 @@ def list_posts(order_by, collation):
     rowstr = "{:>6} | {:>16} | {:>6} | {:<25}"
     # Print a '-' separator with '+' signs at column borders,
     # fill to terminal width
-    separator = "-"*6 + "-+-" + "-"*16 + "-+--------+-"
-    separator += "-"*(tw-len(separator))
+    separator = "-" * 6 + "-+-" + "-" * 16 + "-+--------+-"
+    separator += "-" * (tw - len(separator))
     query = """SELECT post_id, publish_date, title, hidden FROM
     posts ORDER BY publish_date DESC"""
     if order_by == "date" or order_by is None:
         order_by = "publish_date"
     list_text = rowstr.format("ID", "Date", "Hidden", "Title") + "\n"
     list_text += separator + "\n"
-    for row in cur.execute(query):#,(order_by,)):
+    for row in cur.execute(query):  # ,(order_by,)):
         if row["hidden"]:
             hidden = "✔"
         else:
@@ -478,7 +503,7 @@ def edit(id_):
         raise click.BadParameter("No posts found.", param=id_, param_hint="ID")
 
     cur.execute(tagquery, (id_,))
-    tagsline = "{} ".format(tags_text)
+    tagsline = "{} ".format(blog_conf["template"]["tags_line_header"])
     tagslist = (r[0] for r in cur)
     if tagslist is not None:
         tagsline += ", ".join(tagslist)
@@ -489,6 +514,8 @@ def edit(id_):
     if new_content is not None:
         title, body, tags = split_input(new_content)
         cur.execute(updatequery, (title, body, id_))
+        global conn
+        conn.commit()
         db_tagpost(tags, id_)
         rebuild()
     else:
@@ -544,14 +571,15 @@ def rm(id_):
         raise click.BadParameter("No posts found.", param=id_, param_hint="ID")
     else:
         # Remove the file
-        os.remove(path.join(outdir, geturi(fn, pd)))
-        #print(path.join(outdir, geturi(fn, pd)))
+        os.remove(path.join(blog_conf["files"]["blog_dir"], geturi(fn, pd)))
         # Attempt to prune the directory tree the file was in
         try:
-            os.removedirs(path.join(outdir, path.dirname(geturi(fn, pd))))
+            os.removedirs(path.join(blog_conf["files"]["blog_dir"],
+                                    path.dirname(geturi(fn, pd))))
         except OSError:
             pass
         cur.execute("DELETE FROM posts WHERE post_id = ?", (id_,))
+        global conn
         conn.commit()
         click.echo("Deleted post with id {}".format(id_))
         rebuild()
@@ -575,6 +603,7 @@ def bb_import(file):
     Arguments are a list of Markdown files.
     """
     pass
+
 
 for func in post, list_posts, edit, hide, unhide, upload, rm, rebuild, init:
     cli.add_command(func)
